3f4a02f5635803ce93b8807c416ceeb210633b75
Javier Azaret <jazaret@gmail.com>
2017-12-04 09:53:59 -0500

    add templates part 2

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
+	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	})
 
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
